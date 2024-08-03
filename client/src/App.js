2db66340eb03e10f78660de875a5ac26f3caa139
Yash Bhavsar <yashbhavsar3602@gmail.com>
2024-08-02 22:38:37 -0400

    Add Initial and Home Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { GetStarted } from './pages/GetStarted';
+import { Home } from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hackthe6ix
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<GetStarted />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
