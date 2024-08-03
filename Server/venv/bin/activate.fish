439bef0e5131a8eff44533df1294b823639eb99b
Kabir Arora <152303949+KabirArora7@users.noreply.github.com>
2024-08-03 11:35:39 -0400

    Flask (Basic Working)

--- Server/venv/bin/activate.fish
+++ Server/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kabir/Desktop/Hackthe6ix/trackgpt/Server/venv"
+set -gx VIRTUAL_ENV "/Users/kabir/Documents/GitHub/Hackthe6ix-Github/trackgpt/Server/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
