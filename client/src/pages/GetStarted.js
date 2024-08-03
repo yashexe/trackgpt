99a4f0789e3a5ebce23f4c4fe111420bda069435
Kabir Arora <152303949+KabirArora7@users.noreply.github.com>
2024-08-03 12:29:57 -0400

    Create Interface for GetStarted Page React and CSS but need to change black color

--- client/src/pages/GetStarted.js
+++ client/src/pages/GetStarted.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import '/Users/kabir/Documents/GitHub/Hackthe6ix-Github/trackgpt/client/src/Styles/styleGetStarted.css';
+
+
+
 
 export const GetStarted = () => {
     return (
-        <div>
-            <a href='/home'>Get Started Here!</a>
+      <section className="header">
+        <div className="text-box">
+          <h5>Unleash Your Potential</h5>
+          <h1>
+            Get a transcript<br />
+            for your coffee chat<br />
+            No more notes.<br />
+            No more fuss.<br /><br />
+          </h1>
+          <a href="/home" className="hero-btn">Let's get started</a>
         </div>
-    )
-}
+      </section>
+    );
+  };
