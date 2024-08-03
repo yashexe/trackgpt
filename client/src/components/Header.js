1e0d466656886a47f47fd6e1c6b72634f9ddd243
Thiv <xchaos321@gmail.com>
2024-08-03 13:22:47 -0400

    navBar -> Header

---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react"
+import {Typography, Box } from "@mui/material"
+import './Header.css'
+
+ const Header = ()=>{
+
+    return(
+       <div>
+        <Box className='Navigation'>
+            <Typography variant="h6"> Expense Tracker</Typography>
+         </Box>
+        </div>
+
+    )
+}
+
+export default Header;
+
+
+
+
+
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from '../components/Header'; 
+
 
 export const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
+    <div style={{display: 'flex'}}>
+      <Header/>
+      <div className='main-content' >
+        <h1>Home</h1>
+        <p>Welcome to the home page!</p>
+      </div>
     </div>
   );
 };
