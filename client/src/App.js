ef7c570de166e90442e48bbaab11a029b92dd1c2
Thiv <xchaos321@gmail.com>
2024-08-03 10:04:13 -0400

    sideNav initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { GetStarted } from './pages/GetStarted';
+import { Home } from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<GetStarted />} />
+          <Route path="/getInfo" element={<getInfo />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
